290559b652cf39b89aab2ad2deae05299146420b
Yadi Rosadi <yadi.rosadi@BSD-YADI-R-N1.wireless.co.id>
2022-07-11 08:05:16 +0700

    Added my learning file

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-brave-true
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clj_brave_true/core.clj
@@ -0,0 +1,47 @@
+(comment
+  ;; clojure mengenal 2 jenis structure 
+  ;; 1. Literal representations of data structures (like numbers, strings, maps, and vectors)
+  ;; 1, "a string", ["a" "vector" "of" "string"]
+  ;; 2. Operations
+  ;; (operator operand1 operand2.. operandN) 
+  ;; example
+  (+ 1 2 3)
+  ;; operasi bersarang
+  (+ (* 10 5) 50)
+  (str "learn " "clojure " "programming " "language")
+  ;; struktur clojure sangat simple & konsisten jika di compare dgn bahasa lain
+  ;; control flow (if do when)
+  ;; IF
+  ;; bolean-form
+  (if true
+   ;; then-form 
+    "string akan di tampilkan ketika expresi di evaluate"
+    ;; optional-else-form
+    "string ini tidak akan di evaluate")
+  ;; Do, do operator membungkus multiple from ekspresi dan menjalankan masing2 darinya
+  (if true
+    (do (println "Success")
+        "String ini akan ditampilkan setelahnya")
+    (do (println "Failure")
+        "String ini tidak akan ditampilan karena hanya mengevaluate jika kondisinya true"))
+  ;; WHEN, adalah kombinasi (IF + DO) tanpa cabang else
+  (when true
+    (println "success")
+    "String ini akan di evaluate")
+
+  ;; nil, true, false, Truthiness, Equality, and Boolean Expressions
+  ;; nil digunakan untuk identifikasi 'no value'
+  (nil? 1) (nil? nil)
+
+  )
+
+
+
+(ns clj-brave-true.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
